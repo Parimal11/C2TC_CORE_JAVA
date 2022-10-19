67f5401805d58dda0ab1b090e78cdc6d160ea267
Parimal11 <anshuldeshmukh6323@gmail.com>
2022-10-19 13:44:07 +0530

    second commit

--- JavaDemo/src/com/capgemini/javademo/CodeSwitchCaseCharType.java
+++ JavaDemo/src/com/capgemini/javademo/CodeSwitchCaseCharType.java
@@ -18,25 +18,34 @@ public class CodeSwitchCaseCharType {
 		 */
 		switch (alphabet) {
 		
-			//Executes Case 'A' OR case 'a' if alpbhabet  entered is either upper or lower case A
+			//Executes Case 'A' OR case 'a' if albhabet  entered is either upper or lower case A
 			case 'a' : 
 			case 'A' : 
 				
 				//Executes Case 'a' OR A if alphabet entered is A or a 	
-				System.out.println("The value of x is a");
+				System.out.println("The value of alphabet is a");
 				break;//Statements is terminated through break statement
 				
 			//Executes Case 'B' OR case 'b' if alphabet  entered is either upper or lower case B
 			case 'b':
 			case 'B' :
 				
+				//Executes Case 'b' OR B if alphabet entered is B or b
+				System.out.println("The value of aphabet is b");
+				//Statements is terminated through break statement
+				break;
 				
-				System.out.println("The value of x is b");
-				break;
+			//Executes Case 'C' OR case 'c' if alphabet  entered is either upper or lower case C	
 			case 'c':
-			case 'C' :	
-				System.out.println("The value of x is c");
+			case 'C' :
+				
+				//Executes Case 'c' OR C if alphabet entered is C or c 	
+				System.out.println("The value of alpabet is c");
+				
+				
 				break;
+				
+				
 			default:
 				System.out.println("The value of x is other than a,b,c");
 				break;
